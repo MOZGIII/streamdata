894f4bbd355791517293887a66bd912439dfd3a0
MOZGIII <mike-n@narod.ru>
2023-06-11 04:08:09 -0300

    Add tokio-util codec integration

--- crates/streamdata/src/decoder.rs
+++ crates/streamdata/src/decoder.rs
@@ -4,3 +4,5 @@
 pub mod k8s_openapi;
 #[cfg(feature = "serde_json")]
 pub mod serde_json;
+#[cfg(feature = "tokio-util")]
+pub mod tokio_util;
---
+++ crates/streamdata/src/decoder/tokio_util.rs
@@ -0,0 +1,38 @@
+//! [`tokio_util`] integration (i.e. [`tokio_util::codec::Decoder`] support).
+
+/// The decoder that wraps any [`tokio_util::codec::Decoder`].
+#[derive(Debug)]
+pub struct Decoder<T> {
+    /// The inner decoder type.
+    pub inner: T,
+}
+
+impl<T> Decoder<T> {
+    /// Create a new [`Decoder`] with a given [`tokio_util::codec::Decoder`].
+    pub fn new(inner: T) -> Self {
+        Self { inner }
+    }
+}
+
+impl<T> crate::Decoder for Decoder<T>
+where
+    T: tokio_util::codec::Decoder,
+{
+    type Value = <T as tokio_util::codec::Decoder>::Item;
+    type Error = <T as tokio_util::codec::Decoder>::Error;
+
+    fn decode(
+        &mut self,
+        buf: &[u8],
+    ) -> Result<crate::Decoded<Self::Value>, crate::DecodeError<Self::Error>> {
+        let mut buf_bytes = bytes::BytesMut::from(buf);
+        match tokio_util::codec::Decoder::decode(&mut self.inner, &mut buf_bytes) {
+            Ok(None) => Err(crate::DecodeError::NeedMoreData),
+            Ok(Some(value)) => Ok(crate::Decoded {
+                value,
+                consumed_bytes: buf.len() - buf_bytes.len(),
+            }),
+            Err(err) => Err(crate::DecodeError::Other(err)),
+        }
+    }
+}
